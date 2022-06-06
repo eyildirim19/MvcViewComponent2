514d4b40efa5938aceeb2cc5a9dec23df6226638
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-06-06 19:49:10 +0300

    Add project files.

---
+++ MvcViewComponent2/Components/OgrencilerViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcViewComponent2.Components
+{
+    public class OgrencilerViewComponent : ViewComponent
+    {
+        public List<string> ogrenciler = new List<string>();
+
+        public IViewComponentResult Invoke(int? tip)
+        {
+            if (tip == 1) // T
+            {
+                ogrenciler.Add("Recep");
+                ogrenciler.Add("Ekrem");
+            }
+            else if (tip == 2) // Ç
+            {
+                ogrenciler.Add("Emre");
+                ogrenciler.Add("Ahmet");
+            }
+            else
+            {
+                ogrenciler.Add("Bilal");
+                ogrenciler.Add("Alper");
+            }
+
+            return View(ogrenciler); // geriye view dönen bir metot...
+        }
+
+    }
+}
